881a952a9b5b3ea332c535fb9aa0df254847a373
James Watson <james@sitepulse.org>
2017-03-24 15:37:20 +0800

    Changed to web dir

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,11 +2,11 @@
 
 webroot=/www
 
-chown -Rf www:www /www
+cd /www
+chown -Rf www:www *
 
 if [ -f "/www/composer.lock" ]; then
   php composer.phar install --no-dev
 fi
-
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
