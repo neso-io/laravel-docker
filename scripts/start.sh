f57c6600fc5c3f1313e770293a525e64766cf7cc
James Watson <james@sitepulse.org>
2017-03-27 12:09:48 +0800

    Set permission for folders

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,4 +9,8 @@ if [ -f "/www/composer.lock" ]; then
   composer install --no-dev
 fi
 
+sudo chgrp -R www storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
+
+
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
