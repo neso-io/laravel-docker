844074c04399592867672f3f4415b762f554f861
James Watson <james@sitepulse.org>
2017-03-28 10:39:28 +0800

    Write .env is var set

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,12 +5,15 @@ webroot=/www
 cd /www
 chown -Rf www:www *
 
+if [[ -z "${env}" ]]; then
+    echo $env > .env
+fi
+
 if [ -f "/www/composer.lock" ]; then
-  composer install --no-dev
+    composer install --no-dev
 fi
 
 chgrp -R www storage bootstrap/cache
 chmod -R ug+rwx storage bootstrap/cache
-
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
